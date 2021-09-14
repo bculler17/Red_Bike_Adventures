ff67501c4e7f3e7562fa04c05a045903878d6973
blawson1015 <49499193+blawson1015@users.noreply.github.com>
2021-09-14 18:23:40 -0500

    Create rba_javascript.js

---
+++ JavaScript/rba_javascript.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+});
+
+(window).on('beforeunload', function() {
+    (window).scrollTop(0);
+});
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  var m = document.getElementById("map");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+    m.style.display = "none";
+  }
+}
+
